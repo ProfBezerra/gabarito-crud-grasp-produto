5d42ca0337d71f034ec99086a1b520e75b154bcf
profBezerra <bezerra.alcantara@uece.br>
2026-05-16 17:32:39 -0300

    [S2] Criar repositories com JpaRepository #6

---
+++ backend/produtos-api/src/main/java/br/unifor/produtosapi/domain/Produto.java
@@ -0,0 +1,63 @@
+package br.unifor.produtosapi.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+  
+    @Column(nullable = false, length = 150)
+    private String nome;
+  
+    @Column(nullable = false)
+    private Double preco;
+  
+    @ManyToOne
+    @JoinColumn(name = "tipo_id", nullable = false)
+    private TipoProduto tipo;
+  
+    public Produto() {}
+  
+    public Produto(String nome, Double preco, TipoProduto tipo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.tipo = tipo;
+    }
+  
+    // Getters e setters
+    public Long getId() {
+        return id;
+    }
+  
+    public void setId(Long id) {
+        this.id = id;
+    }
+  
+    public String getNome() {
+        return nome;
+    }
+  
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+  
+    public Double getPreco() {
+        return preco;
+    }
+  
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+  
+    public TipoProduto getTipo() {
+        return tipo;
+    }
+  
+    public void setTipo(TipoProduto tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ backend/produtos-api/src/main/java/br/unifor/produtosapi/repository/TipoProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.unifor.produtosapi.repository;
+
+import br.unifor.produtosapi.domain.TipoProduto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoProdutoRepository extends JpaRepository<TipoProduto, Long> {
+}
